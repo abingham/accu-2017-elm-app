00ab4dd654d602c447c7db6022c9ab9c9d8ecd20
Austin Bingham <austin@sixty-north.com>
2017-02-05 16:00:38 +0100

    Working on card view.

--- src/ACCUSchedule/View.elm
+++ src/ACCUSchedule/View.elm
@@ -5,8 +5,12 @@ import ACCUSchedule.Msg as Msg
 import ACCUSchedule.Types as Types
 import Html exposing (a, div, h1, Html, p, text)
 import Html.Attributes exposing (style)
+import Material.Button as Button
+import Material.Card as Card
 import Material.Color as Color
+import Material.Elevation as Elevation
 import Material.Layout as Layout
+import Material.Options as Options exposing (cs, css)
 
 
 dayString : Types.Day -> String
@@ -48,20 +52,52 @@ tabToDay tab =
             Types.Day4
 
 
-proposalView : Types.Proposal -> Html Msg.Msg
-proposalView proposal =
-    div []
-        [ text (proposal.title) ]
+margin2 : Options.Property a b
+margin2 =
+    css "margin" "4px 8px 4px 0px"
+
+
+proposalCard : Types.Proposal -> Html Msg.Msg
+proposalCard proposal =
+    let
+        fullName =
+            \p -> p.firstName ++ " " ++ p.lastName
+
+        presenterNames =
+            List.map fullName proposal.presenters
+
+        presenters =
+            String.join ", " presenterNames
+    in
+        Card.view
+            [ margin2
+              --, Color.background (Color.color Color.DeepPurple Color.S300)
+            , Elevation.e2
+            , css "width" (toString 400 ++ "px")
+            , Card.border
+            ]
+            [ Card.title []
+                ([ Card.head [] [ text proposal.title ]
+                 , Card.subhead [] [ text presenters ]
+                 ]
+                )
+            , Card.text [] [ text proposal.text ]
+            ]
 
 
 proposalsView : Model.Model -> Html Msg.Msg
 proposalsView model =
     let
-        day = tabToDay model.selectedTab
-        pred = \p -> p.day == day
-        proposals = List.filter pred model.proposals
+        day =
+            tabToDay model.selectedTab
+
+        pred =
+            \p -> p.day == day
+
+        proposals =
+            List.filter pred model.proposals
     in
-        List.map proposalView proposals |> div []
+        List.map proposalCard proposals |> div []
 
 
 view : Model.Model -> Html Msg.Msg
