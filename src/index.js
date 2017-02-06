b367aa20dcbda19fe785289c25462c09e362df20
Austin Bingham <austin@sixty-north.com>
2017-02-06 10:06:21 +0100

    Local storage working for starred sessions

--- src/index.js
+++ src/index.js
@@ -11,7 +11,20 @@ require('./index.html');
 var Elm = require('./Main.elm');
 var mountNode = document.getElementById('main');
 
-// TODO: handle "store" port to save starred proposals
+var starredItem = 'accu2017_starred';
 
 // TODO: Load starred proposals from localstorage and pass to embed().
-var app = Elm.ACCUSchedule.embed(mountNode, []);
+var starred = localStorage.getItem(starredItem);
+if (starred) {
+    starred = JSON.parse(starred);
+}
+else {
+    starred = [];
+}
+
+var app = Elm.ACCUSchedule.embed(mountNode, starred);
+
+// handle "store" port to save starred proposals
+app.ports.store.subscribe(function(starred) {
+    localStorage.setItem(starredItem, JSON.stringify(starred));
+});
