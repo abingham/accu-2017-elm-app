f9f0e800d378227e182308d2526652ef8c259c34
Austin Bingham <austin@sixty-north.com>
2017-02-20 09:30:42 +0100

    Added ordinal and toString for QuickieSlots.

--- src/ACCUSchedule/Types/QuickieSlots.elm
+++ src/ACCUSchedule/Types/QuickieSlots.elm
@@ -1,7 +1,36 @@
 module ACCUSchedule.Types.QuickieSlots exposing (..)
 
+
 type QuickieSlot
     = Slot1
     | Slot2
     | Slot3
     | Slot4
+
+
+ordinal : QuickieSlot -> Int
+ordinal slot =
+    case slot of
+        Slot1 ->
+            1
+
+        Slot2 ->
+            2
+
+        Slot3 ->
+            3
+
+        Slot4 ->
+            4
+
+toString : QuickieSlot -> String
+toString slot =
+    case slot of
+        Slot1 ->
+            "Slot 1"
+        Slot2 ->
+            "Slot 2"
+        Slot3 ->
+            "Slot 3"
+        Slot4 ->
+            "Slot 4"
