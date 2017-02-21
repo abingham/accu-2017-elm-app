97a8a7f1f0e06e4690ea8855fc8e8a50a32f61b9
Austin Bingham <austin@sixty-north.com>
2017-02-21 11:31:39 +0100

    Moved dev-server stuff into its own directory.

--- dev-server/api.js
+++ dev-server/api.js
@@ -8,7 +8,7 @@ var server = jsonServer.create()
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(jsonServer.defaults())
 
-var router = jsonServer.router('db.json')
+var router = jsonServer.router('dev-server/db.json')
 server.use(router)
 
 console.log('Listening at 4000')
