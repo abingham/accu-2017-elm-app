11273c6c072203a4cda641be28c69b90812dbdf8
Austin Bingham <austin@sixty-north.com>
2017-02-25 21:13:18 +0000

    Cleaned up dev-server implementation.

--- dev-server/api.js
+++ dev-server/api.js
@@ -1,15 +1,19 @@
 // A simple node app for serving up some dummy test data.
 
-var jsonServer = require('json-server')
+var jsonServer = require('json-server');
 
 // Returns an Express server
-var server = jsonServer.create()
+var server = jsonServer.create();
 
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(jsonServer.defaults())
+server.use(jsonServer.defaults());
 
-var router = jsonServer.router('dev-server/db.json')
-server.use(router)
+var router = jsonServer.router('dev-server/db.json');
+server.use(router);
 
-console.log('Listening at 4000')
-server.listen(4000)
+server.use(jsonServer.rewriter({
+    '/proposals/api/': '/'
+}));
+
+console.log('Listening at 4000');
+server.listen(4000);
