8bb07f022a09d19e6987476b57b2803b6036c952
Austin Bingham <austin@sixty-north.com>
2017-02-25 21:28:12 +0000

    Small fix to dev server.

--- dev-server/api.js
+++ dev-server/api.js
@@ -8,12 +8,12 @@ var server = jsonServer.create();
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(jsonServer.defaults());
 
-var router = jsonServer.router('dev-server/db.json');
-server.use(router);
-
 server.use(jsonServer.rewriter({
     '/proposals/api/': '/'
 }));
+
+var router = jsonServer.router('dev-server/db.json');
+server.use(router);
 
 console.log('Listening at 4000');
 server.listen(4000);
