64355110b8c196c6bf61c14aa37ce8c9074436d0
Austin Bingham <austin@sixty-north.com>
2017-03-10 09:57:29 +0100

    Moved proposalCard into separate module.

--- src/ACCUSchedule/View.elm
+++ src/ACCUSchedule/View.elm
@@ -10,6 +10,7 @@ import ACCUSchedule.Types.Days as Days
 import ACCUSchedule.Types.QuickieSlots as QuickieSlots
 import ACCUSchedule.Types.Rooms as Rooms
 import ACCUSchedule.Types.Sessions as Sessions
+import ACCUSchedule.View.ProposalCard exposing (proposalCard)
 import ACCUSchedule.View.Theme as Theme
 import Html exposing (a, br, div, h1, Html, img, p, text)
 import Html.Attributes exposing (height, href, src)
@@ -21,7 +22,6 @@ import Material.Chip as Chip
 import Material.Color as Color
 import Material.Elevation as Elevation
 import Material.Footer as Footer
-import Material.Icon as Icon
 import Material.Layout as Layout
 import Material.List as Lists
 import Material.Options as Options
@@ -36,11 +36,6 @@ proposalCardGroup =
 
 presenterCardGroup : Int
 presenterCardGroup =
-    1
-
-
-bookmarksControlGroup : Int
-bookmarksControlGroup =
     1
 
 
@@ -140,104 +135,13 @@ presenterCard model presenter =
             ]
 
 
-{-| A card-view of a single proposal. This displays the title, presenters,
-location, and potentially other information about a proposal, though not the
-full text of the abstract. This includes a clickable icon for "starring" a
-propposal.
--}
-proposalCard : Model.Model -> Types.Proposal -> Html Msg.Msg
-proposalCard model proposal =
-    let
-        room =
-            Rooms.toString proposal.room
-
-        time =
-            Sessions.toString proposal.session
-
-        location =
-            String.join ", " <|
-                case proposal.quickieSlot of
-                    Just slot ->
-                        [ time, QuickieSlots.toString slot, room ]
-
-                    _ ->
-                        [ time, room ]
-
-        presenterLink presenter =
-            Layout.link
-                [ Layout.href (Routing.presenterUrl presenter.id) ]
-                [ text <| presenter.firstName ++ " " ++ presenter.lastName ]
-
-        presenterLinks =
-            List.map presenterLink (Model.presenters model proposal)
-                |> List.intersperse (text ", ")
-
-        dayLink =
-            Layout.link
-                [ Layout.href (Routing.dayUrl proposal.day) ]
-                [ text <| Days.toString proposal.day ]
-
-        presenterInfoButton presenter =
-            Button.render Msg.Mdl
-                [ proposalCardGroup
-                , presenterDetailsControlGroup
-                , presenter.id
-                ]
-                model.mdl
-                [ Button.ripple
-                , Button.link <| Routing.presenterUrl presenter.id
-                ]
-                [ text <| Types.fullName presenter ]
-    in
-        Card.view
-            [ if proposal.raised then
-                Elevation.e8
-              else
-                Elevation.e2
-            , Options.css "margin-right" "5px"
-            , Options.css "margin-bottom" "10px"
-            , Options.onMouseEnter (Msg.RaiseProposal True proposal.id)
-            , Options.onMouseLeave (Msg.RaiseProposal False proposal.id)
-            , Color.background Theme.background
-            ]
-            [ Card.actions
-                [ Typo.left
-                ]
-                (List.map
-                    presenterInfoButton
-                    (Model.presenters model proposal)
-                )
-            , Card.title
-                [ Color.text Color.black
-                ]
-                [ Card.head [] [ text proposal.title ]
-                , Card.subhead []
-                    [ dayLink
-                    , text <| ", " ++ location
-                    ]
-                ]
-            , Card.text
-                [ Card.expand ]
-                []
-            , Card.actions
-                [ Color.background Theme.accent
-                , Typo.left
-                , Options.css "justify-content" "space-between"
-                , Options.css "display" "flex"
-                ]
-                [ proposalInfoButton model proposal
-                , bookmarkButton model proposal
-                ]
-            ]
-
-
 flowCardView : Model.Model -> List Types.Proposal -> Html Msg.Msg
 flowCardView model proposals =
     Options.div
         [ Options.css "display" "flex"
         , Options.css "flex-flow" "row wrap"
         ]
-        (List.map (proposalCard model) proposals)
+        (List.map (proposalCard proposalCardGroup model) proposals)
 
 
 sessionView : Model.Model -> List Types.Proposal -> Sessions.Session -> List (Html Msg.Msg)
@@ -335,42 +239,6 @@ agendaView model =
         List.concatMap dview Days.conferenceDays
 
 
-bookmarkButton : Model.Model -> Types.Proposal -> Html Msg.Msg
-bookmarkButton model proposal =
-    let
-        icon =
-            if List.member proposal.id model.bookmarks then
-                "bookmark"
-            else
-                "bookmark_border"
-    in
-        Button.render Msg.Mdl
-            [ proposalCardGroup
-            , bookmarksControlGroup
-            , proposal.id
-            ]
-            model.mdl
-            [ Button.icon
-            , Button.ripple
-            , Options.onClick <| Msg.ToggleBookmark proposal.id
-            ]
-            [ Icon.i icon ]
-
-
-proposalInfoButton : Model.Model -> Types.Proposal -> Html Msg.Msg
-proposalInfoButton model proposal =
-    Button.render Msg.Mdl
-        [ proposalCardGroup
-        , proposalDetailsControlGroup
-        , proposal.id
-        ]
-        model.mdl
-        [ Button.ripple
-        , Button.link <| Routing.proposalUrl proposal.id
-        ]
-        [ text "details" ]
-
-
 {-| Display a single proposal. This includes all of the details of the proposal,
 including the full text of the abstract.
 -}
@@ -395,7 +263,7 @@ proposalView model proposal =
             ]
             [ Options.styled p
                 []
-                [ proposalCard model proposal ]
+                [ proposalCard proposalCardGroup model proposal ]
             , Options.styled p
                 [ Typo.body1
                 , Options.css "width" "30em"
---
+++ src/ACCUSchedule/View/ProposalCard.elm
@@ -0,0 +1,162 @@
+module ACCUSchedule.View.ProposalCard exposing (proposalCard)
+
+import ACCUSchedule.Model as Model
+import ACCUSchedule.Msg as Msg
+import ACCUSchedule.Routing as Routing
+import ACCUSchedule.Types as Types
+import ACCUSchedule.Types.Days as Days
+import ACCUSchedule.Types.QuickieSlots as QuickieSlots
+import ACCUSchedule.Types.Rooms as Rooms
+import ACCUSchedule.Types.Sessions as Sessions
+import ACCUSchedule.View.Theme as Theme
+import Html exposing (a, br, div, h1, Html, img, p, text)
+import Material.Button as Button
+import Material.Card as Card
+import Material.Color as Color
+import Material.Elevation as Elevation
+import Material.Icon as Icon
+import Material.Layout as Layout
+import Material.Options as Options
+import Material.Typography as Typo
+
+
+presenterDetailsControlGroup : Int
+presenterDetailsControlGroup =
+    0
+
+
+proposalDetailsControlGroup : Int
+proposalDetailsControlGroup =
+    1
+
+
+bookmarksControlGroup : Int
+bookmarksControlGroup =
+    2
+
+
+proposalInfoButton : Int -> Model.Model -> Types.Proposal -> Html Msg.Msg
+proposalInfoButton controlGroup model proposal =
+    Button.render Msg.Mdl
+        [ controlGroup
+        , proposalDetailsControlGroup
+        , proposal.id
+        ]
+        model.mdl
+        [ Button.ripple
+        , Button.link <| Routing.proposalUrl proposal.id
+        ]
+        [ text "details" ]
+
+
+bookmarkButton : Int -> Model.Model -> Types.Proposal -> Html Msg.Msg
+bookmarkButton controlGroup model proposal =
+    let
+        icon =
+            if List.member proposal.id model.bookmarks then
+                "bookmark"
+            else
+                "bookmark_border"
+    in
+        Button.render Msg.Mdl
+            [ controlGroup
+            , bookmarksControlGroup
+            , proposal.id
+            ]
+            model.mdl
+            [ Button.icon
+            , Button.ripple
+            , Options.onClick <| Msg.ToggleBookmark proposal.id
+            ]
+            [ Icon.i icon ]
+
+
+{-| A card-view of a single proposal. This displays the title, presenters,
+location, and potentially other information about a proposal, though not the
+full text of the abstract. This includes a clickable icon for "starring" a
+propposal.
+-}
+proposalCard : Int -> Model.Model -> Types.Proposal -> Html Msg.Msg
+proposalCard controlGroup model proposal =
+    let
+        room =
+            Rooms.toString proposal.room
+
+        time =
+            Sessions.toString proposal.session
+
+        location =
+            String.join ", " <|
+                case proposal.quickieSlot of
+                    Just slot ->
+                        [ time, QuickieSlots.toString slot, room ]
+
+                    _ ->
+                        [ time, room ]
+
+        presenterLink presenter =
+            Layout.link
+                [ Layout.href (Routing.presenterUrl presenter.id) ]
+                [ text <| presenter.firstName ++ " " ++ presenter.lastName ]
+
+        presenterLinks =
+            List.map presenterLink (Model.presenters model proposal)
+                |> List.intersperse (text ", ")
+
+        dayLink =
+            Layout.link
+                [ Layout.href (Routing.dayUrl proposal.day) ]
+                [ text <| Days.toString proposal.day ]
+
+        presenterInfoButton presenter =
+            Button.render Msg.Mdl
+                [ controlGroup
+                , presenterDetailsControlGroup
+                , presenter.id
+                ]
+                model.mdl
+                [ Button.ripple
+                , Button.link <| Routing.presenterUrl presenter.id
+                ]
+                [ text <| Types.fullName presenter ]
+    in
+        Card.view
+            [ if proposal.raised then
+                Elevation.e8
+              else
+                Elevation.e2
+            , Options.css "margin-right" "5px"
+            , Options.css "margin-bottom" "10px"
+            , Options.onMouseEnter (Msg.RaiseProposal True proposal.id)
+            , Options.onMouseLeave (Msg.RaiseProposal False proposal.id)
+            , Color.background Theme.background
+            ]
+            [ Card.actions
+                [ Typo.left
+                ]
+                (List.map
+                    presenterInfoButton
+                    (Model.presenters model proposal)
+                )
+            , Card.title
+                [ Color.text Color.black
+                ]
+                [ Card.head [] [ text proposal.title ]
+                , Card.subhead []
+                    [ dayLink
+                    , text <| ", " ++ location
+                    ]
+                ]
+            , Card.text
+                [ Card.expand ]
+                []
+            , Card.actions
+                [ Color.background Theme.accent
+                , Typo.left
+                , Options.css "justify-content" "space-between"
+                , Options.css "display" "flex"
+                ]
+                [ proposalInfoButton controlGroup model proposal
+                , bookmarkButton controlGroup model proposal
+                ]
+            ]
