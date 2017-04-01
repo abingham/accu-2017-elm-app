94bd59be2072f968724222282877ab149bcb87e8
Austin Bingham <austin@sixty-north.com>
2017-04-01 18:04:25 +0200

    Using a canned database specifically for tests.

---
+++ tests/api-server/api.js
@@ -0,0 +1,19 @@
+// A simple node app for serving up some dummy test data.
+
+var jsonServer = require('json-server');
+
+// Returns an Express server
+var server = jsonServer.create();
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(jsonServer.defaults());
+
+server.use(jsonServer.rewriter({
+    '/proposals/api/': '/'
+}));
+
+var router = jsonServer.router('tests/api-server/db.json');
+server.use(router);
+
+console.log('Listening at 4000');
+server.listen(4000);
